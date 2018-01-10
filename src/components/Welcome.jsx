2c3458414afcd73c20314b6ed1b9988154c09414
twalk4821 <tyjohnwalker@gmail.com>
2018-01-09 16:07:23 -0800

    fade in welcome text

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
+import './Welcome.css';
+
 const welcomeText = "Welcome! My name is Tyler.";
 
-const style = {
-  marginTop: '22vh',
-};
-
 const Welcome = () => (
-  <h1 style={style}>{welcomeText}</h1>
+  <h1 className='welcome'>{welcomeText}</h1>
 );
 
 export default Welcome;
